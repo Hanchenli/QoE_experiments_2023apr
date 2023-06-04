1938471b76da30a0836341174bf6199776512463
Hanchen Li <lihanc2002@uchicago.edu>
2023-06-04 17:41:30 -0500

    ems

--- app.js
+++ app.js
@@ -58,8 +58,8 @@ app.use(templating('views', {
 // add controller:
 app.use(controller());
 
-app.listen(Number(process.argv[2]) || 3002);
-console.log('app started at port' + String(Number(process.argv[2]) || 3002));
+app.listen(Number(process.argv[2]) || 3001);
+console.log('app started at port' + String(Number(process.argv[2]) || 3001));
 
 function parseUser(obj) {
     if (!obj) {
